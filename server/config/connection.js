45c8e0b45f97c308e19d35cf5379a5a89c61fd3c
Haillander <haillanderviana@gmail.com>
2023-04-26 20:01:02 +1000

    deploying on heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-books.books', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/deep-books', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
